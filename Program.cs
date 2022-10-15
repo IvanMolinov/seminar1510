9bdf0c942f6f9c677d0832dd34ac2584a277c740
Ivan Molinov <ivan.molinov@mail.ru>
2022-10-15 12:01:34 +0300

    2 и 3 задачи

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
+﻿// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
 
-if (a == b * b)
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Нет");
-}
+// if (a == b * b)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+//Задача 2
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int i = -N; i <= N; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+//Задача 3
+int N = Convert.ToInt32(Console.ReadLine());
+int answer = N % 10;
+Console.WriteLine(answer);
 
